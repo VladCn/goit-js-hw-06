96d3c5b944b8506e4091a4771b485ffdbaff727f
Vladislav Zaitsev <vladislav.zaytcev84@gmail.com>
2021-10-26 13:10:07 +0300

    hw 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,22 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+// const ingredientEl = document.createElement(`li`);
+// ingredientEl.textContent =`1111`;
+// ingredientEl.classList.add(`item`);
+//
+// console.log(ingredientEl)
+
+const makeArrayIngredients = ingredients.map( ingredient => {
+  console.log(ingredient)
+  const ingredientEl = document.createElement(`li`);
+  ingredientEl.textContent = ingredient;
+  ingredientEl.classList.add(`item`);
+  return ingredientEl;
+
+})
+
+console.log(makeArrayIngredients)
+
+const conteinerIngredientEl = document.querySelector(`#ingredients`)
+conteinerIngredientEl.append(...makeArrayIngredients);
