4efcef53d708af4a25c13730e77dca0d4f22fc95
Vladislav Zaitsev <vladislav.zaytcev84@gmail.com>
2021-10-30 19:02:25 +0300

    hw 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,25 @@
+const formJs = document.querySelector(".login-form")
 
+const passwordJs = document.querySelector('input[type="password"]')
+const emailJs = document.querySelector('input[type="email"]')
+const buttonJs = document.querySelector('input[type="submit"]')
+
+const inEmail = (event) => {
+
+    console.log(event.target)
+    console.log(passwordJs.getAttribute("name"))
+    event.preventDefault();
+
+    if(!passwordJs.value || !emailJs.value){
+    alert("все поля должны быть заполнены")
+    } else {
+        console.log({
+            [passwordJs.getAttribute("name")]: passwordJs.value,
+            [emailJs.getAttribute("name")]: emailJs.value
+        })
+        formJs.reset()
+    }
+}
+formJs.addEventListener("submit", inEmail)
+
+console.log(emailJs.innerHTML)
