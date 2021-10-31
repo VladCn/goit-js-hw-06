158468f08315164d272421571084094653c31724
Vladislav Zaitsev <vladislav.zaytcev84@gmail.com>
2021-10-31 14:01:15 +0200

    hw 10

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,44 @@
+const createBtnJs = document.querySelector('button[data-create]')
+const destroyBtnJs = document.querySelector('button[data-destroy]')
+const inputJs = document.querySelector('input')
+const divJs = document.querySelector('#boxes')
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function getInputValue () {
+
+ const valueInput = inputJs.value
+  createBoxes(valueInput)
+}
+function createBoxes(amount){
+
+  let height = 30
+  let width = 30
+
+  for(let i = 0; i <= amount; i += 1){
+    const divCreate = document.createElement('div')
+    divCreate.style.height = `${height}px`
+    divCreate.style.width = `${width}px`
+    divCreate.style.backgroundColor = getRandomHexColor()
+    divCreate.classList.add("newDiv")
+    divJs.insertAdjacentElement("beforeend", divCreate)
+    height += 10
+    width += 10
+  }
+}
+
+function reset(){
+const divArrayJs = document.querySelectorAll(".newDiv")
+  for(const value of divArrayJs) {
+    value.remove()
+  }
+}
+
+
+createBtnJs.addEventListener('click', getInputValue)
+destroyBtnJs.addEventListener('click', reset)
+
+
