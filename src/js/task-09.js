66be6e28f97e196983cfdc553f7e235c0d0b40b7
Vladislav Zaitsev <vladislav.zaytcev84@gmail.com>
2021-10-31 12:17:15 +0200

    hw 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtn = document.querySelector(".change-color")
+const textColorJs = document.querySelector(".color")
+const bodyJS = document.querySelector("body")
+
+const action = () => {
+  getRandomHexColor()
+  textColorJs.textContent = getRandomHexColor()
+  bodyJS.style.backgroundColor = `${textColorJs.textContent}`
+}
+
+const btnAction = changeColorBtn.addEventListener("click", action)
